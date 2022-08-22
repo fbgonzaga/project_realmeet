81a2dc59a5a3eecbf8f24d6ca98621a3232e79ab
Flavio Gonzaga <fbgonzaga@gmail.com>
2022-08-21 23:47:20 -0300

    first commit

---
+++ src/test/java/br/com/sw2you/realmeet/RealmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sw2you.realmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
