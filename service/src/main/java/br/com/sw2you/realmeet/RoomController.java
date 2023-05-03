742aa9ff04ba110f3196fd4c0e45b7c85cb529c1
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-03 00:46:57 -0300

    Adjust formatting to conform to prettier plugin.

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -1,16 +1,16 @@
 package br.com.sw2you.realmeet;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
 import br.com.sw2you.realmeet.api.model.Room;
+import java.util.concurrent.CompletableFuture;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.CompletableFuture;
-
-import static java.util.concurrent.CompletableFuture.supplyAsync;
-
 @RestController
 public class RoomController implements RoomsApi {
+
     @Override
     public CompletableFuture<ResponseEntity<Room>> listRooms(Long id) {
         return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
