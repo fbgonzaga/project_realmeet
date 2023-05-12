6dc2b5318ee927867bf58241b6cca43102647dd4
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-11 23:26:30 -0300

    Removing unused imports. Bug fix on AllocationRepository.

--- service/src/main/java/br/com/sw2you/realmeet/service/AllocationService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/AllocationService.java
@@ -1,17 +1,11 @@
 package br.com.sw2you.realmeet.service;
 
-import static java.util.Objects.requireNonNull;
-
-import br.com.sw2you.realmeet.api.model.*;
-import br.com.sw2you.realmeet.domain.entity.Room;
+import br.com.sw2you.realmeet.api.model.AllocationDTO;
+import br.com.sw2you.realmeet.api.model.CreateAllocationDTO;
 import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
-import br.com.sw2you.realmeet.exception.RoomNotFoundException;
 import br.com.sw2you.realmeet.mapper.AllocationMapper;
-import br.com.sw2you.realmeet.mapper.RoomMapper;
-import br.com.sw2you.realmeet.validator.RoomValidator;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AllocationService {
