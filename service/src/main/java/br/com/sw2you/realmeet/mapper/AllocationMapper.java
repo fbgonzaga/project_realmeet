91dc225319d6156e0f61476ca440ecf26f0d88c4
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-14 21:48:26 -0300

    Test and validate allocation.

--- service/src/main/java/br/com/sw2you/realmeet/mapper/AllocationMapper.java
+++ service/src/main/java/br/com/sw2you/realmeet/mapper/AllocationMapper.java
@@ -4,7 +4,6 @@ import br.com.sw2you.realmeet.api.model.AllocationDTO;
 import br.com.sw2you.realmeet.api.model.CreateAllocationDTO;
 import br.com.sw2you.realmeet.domain.entity.Allocation;
 import br.com.sw2you.realmeet.domain.entity.Room;
-import br.com.sw2you.realmeet.domain.model.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -20,5 +19,6 @@ public interface AllocationMapper {
 
     @Mapping(source = "employee.name", target = "employeeName")
     @Mapping(source = "employee.email", target = "employeeEmail")
+    @Mapping(source = "room.id", target = "roomId")
     AllocationDTO fromEntityToAllocationDTO(Allocation allocation);
 }
