61b1e079f6ab2db9ff53bc5e0d537db059c3c5d2
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-14 21:48:26 -0300

    Test and validate allocation.

--- service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
@@ -1,18 +1,10 @@
 package br.com.sw2you.realmeet.validator;
 
-import static br.com.sw2you.realmeet.util.DateUtils.*;
 import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static br.com.sw2you.realmeet.validator.ValidatorUtils.*;
 
 import br.com.sw2you.realmeet.api.model.CreateAllocationDTO;
-import br.com.sw2you.realmeet.api.model.UpdateAllocationDTO;
-import br.com.sw2you.realmeet.api.model.UpdateRoomDTO;
 import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
-import br.com.sw2you.realmeet.util.DateUtils;
-import java.time.Duration;
-import java.time.OffsetDateTime;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,109 +17,12 @@ public class AllocationValidator {
 
     public void validate(CreateAllocationDTO createAllocationDTO) {
         var validationErrors = new ValidationErrors();
-
         validateSubject(createAllocationDTO.getSubject(), validationErrors);
-        validateEmployeeName(createAllocationDTO.getEmployeeName(), validationErrors);
-        validateEmployeeEmail(createAllocationDTO.getEmployeeEmail(), validationErrors);
-        validateDates(
-            createAllocationDTO.getRoomId(),
-            createAllocationDTO.getStartAt(),
-            createAllocationDTO.getEndAt(),
-            validationErrors
-        );
-
-        throwOnError(validationErrors);
-    }
-
-    public void validate(Long allocationId, Long roomId, UpdateAllocationDTO updateAllocationDTO) {
-        var validationErrors = new ValidationErrors();
-
-        validateRequired(allocationId, ALLOCATION_ID, validationErrors);
-        validateSubject(updateAllocationDTO.getSubject(), validationErrors);
-        validateDates(roomId, updateAllocationDTO.getStartAt(), updateAllocationDTO.getEndAt(), validationErrors);
-
         throwOnError(validationErrors);
     }
 
     private void validateSubject(String subject, ValidationErrors validationErrors) {
         validateRequired(subject, ALLOCATION_SUBJECT, validationErrors);
         validateMaxLength(subject, ALLOCATION_SUBJECT, ALLOCATION_SUBJECT_MAX_LENGTH, validationErrors);
-    }
-
-    private void validateEmployeeName(String employeeName, ValidationErrors validationErrors) {
-        validateRequired(employeeName, ALLOCATION_EMPLOYEE_NAME, validationErrors);
-        validateMaxLength(
-            employeeName,
-            ALLOCATION_EMPLOYEE_NAME,
-            ALLOCATION_EMPLOYEE_NAME_MAX_LENGTH,
-            validationErrors
-        );
-    }
-
-    private void validateEmployeeEmail(String employeeEmail, ValidationErrors validationErrors) {
-        validateRequired(employeeEmail, ALLOCATION_EMPLOYEE_EMAIL, validationErrors);
-        validateMaxLength(
-            employeeEmail,
-            ALLOCATION_EMPLOYEE_EMAIL,
-            ALLOCATION_EMPLOYEE_EMAIL_MAX_LENGTH,
-            validationErrors
-        );
-    }
-
-    private void validateDates(
-        Long roomId,
-        OffsetDateTime startAt,
-        OffsetDateTime endAt,
-        ValidationErrors validationErrors
-    ) {
-        if (validateDatesPresent(startAt, endAt, validationErrors)) {
-            validateDateOrdering(startAt, endAt, validationErrors);
-            validateDateInTheFuture(startAt, validationErrors);
-            validateDuration(startAt, endAt, validationErrors);
-            validateIfTimeAvailable(roomId, startAt, endAt, validationErrors);
-        }
-    }
-
-    private boolean validateDatesPresent(
-        OffsetDateTime startAt,
-        OffsetDateTime endAt,
-        ValidationErrors validationErrors
-    ) {
-        return (
-            validateRequired(startAt, ALLOCATION_START_AT, validationErrors) &&
-            validateRequired(endAt, ALLOCATION_END_AT, validationErrors)
-        );
-    }
-
-    private void validateDateOrdering(OffsetDateTime startAt, OffsetDateTime endAt, ValidationErrors validationErrors) {
-        if (startAt.isEqual(endAt) || startAt.isAfter(endAt)) {
-            validationErrors.add(ALLOCATION_START_AT, ALLOCATION_START_AT + INCONSISTENT);
-        }
-    }
-
-    private void validateDateInTheFuture(OffsetDateTime date, ValidationErrors validationErrors) {
-        if (date.isBefore(now())) {
-            validationErrors.add(ALLOCATION_START_AT, ALLOCATION_START_AT + IN_THE_PAST);
-        }
-    }
-
-    private void validateDuration(OffsetDateTime startAt, OffsetDateTime endAt, ValidationErrors validationErrors) {
-        if (Duration.between(startAt, endAt).getSeconds() > ALLOCATION_MAX_DURATION_SECONDS) {
-            validationErrors.add(ALLOCATION_END_AT, ALLOCATION_END_AT + EXCEEDS_DURATION);
-        }
-    }
-
-    private void validateIfTimeAvailable(
-        Long roomId,
-        OffsetDateTime startAt,
-        OffsetDateTime endAt,
-        ValidationErrors validationErrors
-    ) {
-        allocationRepository
-            .findAllWithFilters(null, roomId, now(), endAt)
-            .stream()
-            .filter(a -> DateUtils.isOverlapping(startAt, endAt, a.getStartAt(), a.getEndAt()))
-            .findFirst()
-            .ifPresent(__ -> validationErrors.add(ALLOCATION_START_AT, ALLOCATION_START_AT + OVERLAPS));
     }
 }
--- service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiIntegrationTest.java
@@ -1,19 +1,13 @@
 package br.com.sw2you.realmeet.integration;
 
-import static br.com.sw2you.realmeet.utils.TestConstants.*;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import br.com.sw2you.realmeet.api.facade.AllocationApi;
 import br.com.sw2you.realmeet.core.BaseIntegrationTest;
-import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
-import br.com.sw2you.realmeet.email.EmailSender;
-import br.com.sw2you.realmeet.util.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.client.HttpClientErrorException;
 
 class AllocationApiIntegrationTest extends BaseIntegrationTest {
     @Autowired
@@ -21,12 +15,6 @@ class AllocationApiIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     RoomRepository roomRepository;
-
-    @Autowired
-    AllocationRepository allocationRepository;
-
-    @MockBean
-    private EmailSender emailSender;
 
     @Override
     protected void setupEach() throws Exception {
@@ -37,7 +25,7 @@ class AllocationApiIntegrationTest extends BaseIntegrationTest {
     void testCreateAllocationSuccess() {
         var room = roomRepository.saveAndFlush(newRoomBuilder().build());
         var createAllocationDTO = newCreateAllocationDTO().roomId(room.getId());
-        var allocationDTO = api.createAllocation(TEST_CLIENT_API_KEY, createAllocationDTO);
+        var allocationDTO = api.createAllocation(createAllocationDTO);
 
         assertNotNull(allocationDTO.getId());
         assertEquals(room.getId(), allocationDTO.getRoomId());
@@ -46,94 +34,5 @@ class AllocationApiIntegrationTest extends BaseIntegrationTest {
         assertEquals(createAllocationDTO.getEmployeeEmail(), allocationDTO.getEmployeeEmail());
         assertTrue(createAllocationDTO.getStartAt().isEqual(allocationDTO.getStartAt()));
         assertTrue(createAllocationDTO.getEndAt().isEqual(allocationDTO.getEndAt()));
-    }
-
-    @Test
-    void testCreateAllocationValidationError() {
-        var room = roomRepository.saveAndFlush(newRoomBuilder().build());
-        var createAllocationDTO = newCreateAllocationDTO().roomId(room.getId()).subject(null);
-
-        assertThrows(
-            HttpClientErrorException.UnprocessableEntity.class,
-            () -> api.createAllocation(TEST_CLIENT_API_KEY, createAllocationDTO)
-        );
-    }
-
-    @Test
-    void testCreateAllocationWhenRoomDoesNotExist() {
-        assertThrows(
-            HttpClientErrorException.NotFound.class,
-            () -> api.createAllocation(TEST_CLIENT_API_KEY, newCreateAllocationDTO())
-        );
-    }
-
-    @Test
-    void testDeleteAllocationSuccess() {
-        var room = roomRepository.saveAndFlush(newRoomBuilder().build());
-        var allocation = allocationRepository.saveAndFlush(newAllocationBuilder(room).build());
-
-        api.deleteAllocation(TEST_CLIENT_API_KEY, allocation.getId());
-        assertFalse(allocationRepository.findById(allocation.getId()).isPresent());
-    }
-
-    @Test
-    void testDeleteAllocationInThePast() {
-        var room = roomRepository.saveAndFlush(newRoomBuilder().build());
-        var allocation = allocationRepository.saveAndFlush(
-            newAllocationBuilder(room)
-                .withStartAt(DateUtils.now().minusDays(1))
-                .withEndAt(DateUtils.now().minusDays(1).plusHours(1))
-                .build()
-        );
-
-        assertThrows(
-            HttpClientErrorException.UnprocessableEntity.class,
-            () -> api.deleteAllocation(TEST_CLIENT_API_KEY, allocation.getId())
-        );
-    }
-
-    @Test
-    void testDeleteAllocationDoesNotExist() {
-        assertThrows(HttpClientErrorException.NotFound.class, () -> api.deleteAllocation(TEST_CLIENT_API_KEY, 1L));
-    }
-
-    @Test
-    void testUpdateAllocationSuccess() {
-        var room = roomRepository.saveAndFlush(newRoomBuilder().build());
-        var createAllocationDTO = newCreateAllocationDTO().roomId(room.getId());
-        var allocationDTO = api.createAllocation(TEST_CLIENT_API_KEY, createAllocationDTO);
-        var updateAllocationDTO = newUpdateAllocationDTO()
-            .subject(DEFAULT_ALLOCATION_SUBJECT + "_")
-            .startAt(DEFAULT_ALLOCATION_START_AT.plusDays(1))
-            .endAt(DEFAULT_ALLOCATION_END_AT.plusDays(1));
-
-        api.updateAllocation(TEST_CLIENT_API_KEY, allocationDTO.getId(), updateAllocationDTO);
-
-        var allocation = allocationRepository.findById(allocationDTO.getId()).orElseThrow();
-
-        assertEquals(updateAllocationDTO.getSubject(), allocation.getSubject());
-        assertTrue(updateAllocationDTO.getStartAt().isEqual(allocation.getStartAt()));
-        assertTrue(updateAllocationDTO.getEndAt().isEqual(allocation.getEndAt()));
-    }
-
-    @Test
-    void testUpdateAllocationDoesNotExist() {
-        assertThrows(
-            HttpClientErrorException.NotFound.class,
-            () -> api.updateAllocation(TEST_CLIENT_API_KEY, 1L, newUpdateAllocationDTO())
-        );
-    }
-
-    @Test
-    void testUpdateAllocationValidationError() {
-        var room = roomRepository.saveAndFlush(newRoomBuilder().build());
-        var createAllocationDTO = newCreateAllocationDTO().roomId(room.getId());
-        var allocationDTO = api.createAllocation(TEST_CLIENT_API_KEY, createAllocationDTO);
-
-        assertThrows(
-            HttpClientErrorException.UnprocessableEntity.class,
-            () ->
-                api.updateAllocation(TEST_CLIENT_API_KEY, allocationDTO.getId(), newUpdateAllocationDTO().subject(null))
-        );
     }
 }
