4b5b5824b0f60a485a583ca9438281686d146de3
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-23 15:43:36 -0300

    Validate allocation.

--- service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/AllocationValidator.java
@@ -8,7 +8,6 @@ import br.com.sw2you.realmeet.api.model.CreateAllocationDTO;
 import br.com.sw2you.realmeet.api.model.UpdateAllocationDTO;
 import br.com.sw2you.realmeet.api.model.UpdateRoomDTO;
 import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
-import br.com.sw2you.realmeet.util.DateUtils;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import org.springframework.data.domain.PageRequest;
---
+++ service/src/test/java/br/com/sw2you/realmeet/unit/AllocationValidatorUnitTest.java
@@ -0,0 +1,196 @@
+package br.com.sw2you.realmeet.unit;
+
+import static br.com.sw2you.realmeet.utils.TestDataCreator.*;
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
+import static java.time.OffsetDateTime.*;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.com.sw2you.realmeet.core.BaseUnitTest;
+import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
+import br.com.sw2you.realmeet.exception.InvalidRequestException;
+import br.com.sw2you.realmeet.validator.AllocationValidator;
+import br.com.sw2you.realmeet.validator.ValidationError;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class AllocationValidatorUnitTest extends BaseUnitTest {
+    private AllocationValidator victim;
+
+    @Mock
+    private AllocationRepository allocationRepository;
+
+    @BeforeEach
+    void setupEach() {
+        victim = new AllocationValidator(allocationRepository);
+    }
+
+    @Test
+    void testValidateWhenAllocationIsValid() {
+        victim.validate(newCreateAllocationDTO());
+    }
+
+    @Test
+    void testValidateWhenSubjectIsMissing() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().subject(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_SUBJECT, ALLOCATION_SUBJECT + MISSING),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenSubjectExceedsLength() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () ->
+                victim.validate(
+                    newCreateAllocationDTO().subject(StringUtils.rightPad("", ALLOCATION_SUBJECT_MAX_LENGTH + 1, "x"))
+                )
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_SUBJECT, ALLOCATION_SUBJECT + EXCEEDS_MAX_LENGTH),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenEmployeeNameIsMissing() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().employeeName(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_EMPLOYEE_NAME, ALLOCATION_EMPLOYEE_NAME + MISSING),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenEmployeeNameExceedsLength() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () ->
+                victim.validate(
+                    newCreateAllocationDTO()
+                        .employeeName(StringUtils.rightPad("", ALLOCATION_EMPLOYEE_NAME_MAX_LENGTH + 1, "x"))
+                )
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_EMPLOYEE_NAME, ALLOCATION_EMPLOYEE_NAME + EXCEEDS_MAX_LENGTH),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenEmailIsMissing() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().employeeEmail(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_EMPLOYEE_EMAIL, ALLOCATION_EMPLOYEE_EMAIL + MISSING),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenEmailExceedsLength() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () ->
+                victim.validate(
+                    newCreateAllocationDTO()
+                        .employeeEmail(StringUtils.rightPad("", ALLOCATION_EMPLOYEE_EMAIL_MAX_LENGTH + 1, "x"))
+                )
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_EMPLOYEE_EMAIL, ALLOCATION_EMPLOYEE_EMAIL + EXCEEDS_MAX_LENGTH),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenStartAtMissing() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().startAt(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_START_AT, ALLOCATION_START_AT + MISSING),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenEndAtMissing() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().endAt(null))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_END_AT, ALLOCATION_END_AT + MISSING),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenDateOrderIsInvalid() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () ->
+                victim.validate(
+                    newCreateAllocationDTO().startAt(now().plusDays(1)).endAt(now().plusDays(1).minusMinutes(30))
+                )
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_START_AT, ALLOCATION_START_AT + INCONSISTENT),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenIsInThePast() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () -> victim.validate(newCreateAllocationDTO().startAt(now().minusMinutes(30)).endAt(now().plusMinutes(30)))
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_START_AT, ALLOCATION_START_AT + IN_THE_PAST),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+
+    @Test
+    void testValidateWhenDateIntervalExceedsMaxDuration() {
+        var exception = assertThrows(
+            InvalidRequestException.class,
+            () ->
+                victim.validate(
+                    newCreateAllocationDTO()
+                        .startAt(now().plusDays(1))
+                        .endAt(now().plusDays(1).plusSeconds(ALLOCATION_MAX_DURATION_SECONDS + 1))
+                )
+        );
+        assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
+        assertEquals(
+            new ValidationError(ALLOCATION_END_AT, ALLOCATION_END_AT + EXCEEDS_DURATION),
+            exception.getValidationErrors().getError(0)
+        );
+    }
+}
