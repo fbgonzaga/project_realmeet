bacb7c7337e34585a68a8cafa09ce533d88b6586
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-26 00:22:55 -0300

    Endpoint Allocation finished.

--- service/src/test/java/br/com/sw2you/realmeet/unit/AllocationUpdateValidatorUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/AllocationUpdateValidatorUnitTest.java
@@ -1,6 +1,7 @@
 package br.com.sw2you.realmeet.unit;
 
 import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ALLOCATION_ID;
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newUpdateAllocationDTO;
 import static br.com.sw2you.realmeet.validator.ValidatorConstants.*;
 import static java.time.OffsetDateTime.now;
@@ -30,14 +31,14 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
 
     @Test
     void testValidateWhenAllocationIsValid() {
-        victim.validate(DEFAULT_ALLOCATION_ID, newUpdateAllocationDTO());
+        victim.validate(DEFAULT_ALLOCATION_ID, DEFAULT_ROOM_ID, newUpdateAllocationDTO());
     }
 
     @Test
     void testValidateWhenAllocationIdIsMissing() {
         var exception = assertThrows(
             InvalidRequestException.class,
-            () -> victim.validate(null, newUpdateAllocationDTO())
+            () -> victim.validate(null, DEFAULT_ROOM_ID, newUpdateAllocationDTO())
         );
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(
@@ -50,7 +51,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
     void testValidateWhenSubjectIsMissing() {
         var exception = assertThrows(
             InvalidRequestException.class,
-            () -> victim.validate(DEFAULT_ALLOCATION_ID, newUpdateAllocationDTO().subject(null))
+            () -> victim.validate(DEFAULT_ALLOCATION_ID, DEFAULT_ROOM_ID, newUpdateAllocationDTO().subject(null))
         );
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(
@@ -66,6 +67,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
             () ->
                 victim.validate(
                     DEFAULT_ALLOCATION_ID,
+                    DEFAULT_ROOM_ID,
                     newUpdateAllocationDTO().subject(StringUtils.rightPad("", ALLOCATION_SUBJECT_MAX_LENGTH + 1, "x"))
                 )
         );
@@ -80,7 +82,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
     void testValidateWhenStartAtMissing() {
         var exception = assertThrows(
             InvalidRequestException.class,
-            () -> victim.validate(DEFAULT_ALLOCATION_ID, newUpdateAllocationDTO().startAt(null))
+            () -> victim.validate(DEFAULT_ALLOCATION_ID, DEFAULT_ROOM_ID, newUpdateAllocationDTO().startAt(null))
         );
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(
@@ -93,7 +95,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
     void testValidateWhenEndAtMissing() {
         var exception = assertThrows(
             InvalidRequestException.class,
-            () -> victim.validate(DEFAULT_ALLOCATION_ID, newUpdateAllocationDTO().endAt(null))
+            () -> victim.validate(DEFAULT_ALLOCATION_ID, DEFAULT_ROOM_ID, newUpdateAllocationDTO().endAt(null))
         );
         assertEquals(1, exception.getValidationErrors().getNumberOfErrors());
         assertEquals(
@@ -109,6 +111,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
             () ->
                 victim.validate(
                     DEFAULT_ALLOCATION_ID,
+                    DEFAULT_ROOM_ID,
                     newUpdateAllocationDTO().startAt(now().plusDays(1)).endAt(now().plusDays(1).minusMinutes(30))
                 )
         );
@@ -126,6 +129,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
             () ->
                 victim.validate(
                     DEFAULT_ALLOCATION_ID,
+                    DEFAULT_ROOM_ID,
                     newUpdateAllocationDTO().startAt(now().minusMinutes(30)).endAt(now().plusMinutes(30))
                 )
         );
@@ -143,6 +147,7 @@ public class AllocationUpdateValidatorUnitTest extends BaseUnitTest {
             () ->
                 victim.validate(
                     DEFAULT_ALLOCATION_ID,
+                    DEFAULT_ROOM_ID,
                     newUpdateAllocationDTO()
                         .startAt(now().plusDays(1))
                         .endAt(now().plusDays(1).plusSeconds(ALLOCATION_MAX_DURATION_SECONDS + 1))
