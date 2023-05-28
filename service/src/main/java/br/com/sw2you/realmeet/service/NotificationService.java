37d78312a7e528a7c60d6ec3c6098572c91b8fd8
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-28 01:11:48 -0300

    Add email support.

--- service/src/main/java/br/com/sw2you/realmeet/service/NotificationService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/NotificationService.java
@@ -1,43 +1,26 @@
 package br.com.sw2you.realmeet.service;
 
-import static br.com.sw2you.realmeet.util.Constants.ALLOCATION;
-
 import br.com.sw2you.realmeet.domain.entity.Allocation;
-import br.com.sw2you.realmeet.email.EmailInfoBuilder;
 import br.com.sw2you.realmeet.email.EmailSender;
-import br.com.sw2you.realmeet.email.TemplateType;
-import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
     private final EmailSender emailSender;
-    private final EmailInfoBuilder emailInfoBuilder;
 
-    public NotificationService(EmailSender emailSender, EmailInfoBuilder emailInfoBuilder) {
+    public NotificationService(EmailSender emailSender) {
         this.emailSender = emailSender;
-        this.emailInfoBuilder = emailInfoBuilder;
     }
 
     public void notifyAllocationCreated(Allocation allocation) {
-        notify(allocation, TemplateType.ALLOCATION_CREATED);
+        //TODO
     }
 
     public void notifyAllocationUpdated(Allocation allocation) {
-        notify(allocation, TemplateType.ALLOCATION_UPDATED);
+        //TODO
     }
 
     public void notifyAllocationDeleted(Allocation allocation) {
-        notify(allocation, TemplateType.ALLOCATION_DELETED);
-    }
-
-    private void notify(Allocation allocation, TemplateType templateType) {
-        emailSender.send(
-            emailInfoBuilder.createEmailInfo(
-                allocation.getEmployee().getEmail(),
-                templateType,
-                Map.of(ALLOCATION, allocation)
-            )
-        );
+        //TODO
     }
 }
