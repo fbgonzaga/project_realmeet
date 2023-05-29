efeba91ee16c1b9dacec419ee94d678daab0d580
Flavio Gonzaga <fbgonzaga@gmail.com>
2023-05-28 22:32:13 -0300

    Developing reports -- part 1.

--- service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiIntegrationTest.java
@@ -8,9 +8,11 @@ import br.com.sw2you.realmeet.api.facade.AllocationApi;
 import br.com.sw2you.realmeet.core.BaseIntegrationTest;
 import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.email.EmailSender;
 import br.com.sw2you.realmeet.util.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.client.HttpClientErrorException;
 
 class AllocationApiIntegrationTest extends BaseIntegrationTest {
@@ -22,6 +24,9 @@ class AllocationApiIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     AllocationRepository allocationRepository;
+
+    @MockBean
+    private EmailSender emailSender;
 
     @Override
     protected void setupEach() throws Exception {
